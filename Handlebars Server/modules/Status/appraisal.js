da3f737b37dd46d6716ae9f0fc1212199848c370
Aipow <u12081622@tuks.co.za>
2015-03-21 20:38:16 +0200

    Skeleton of what function and variable names to use,raw work

---
+++ Handlebars Server/modules/Status/appraisal.js
@@ -0,0 +1,49 @@
+
+// member variables for appraisal
+var name;
+var description;
+var notRatedIcon;
+var activity ;
+
+//appraisalRating
+var ratingName;
+var rating;
+var ratingIcon;
+
+//AppraisalDefinition
+var appraisalName;
+var description;
+var notRatedIcon;
+
+
+
+
+
+//functions 
+function activateAppraisalType () {
+	// body...
+
+}
+
+function createAppraisalType()
+{
+
+}
+
+function assignAppraisalToPost () {
+	// body...
+
+}
+
+//Helper function
+//openingg db to save data or retrieve
+function openDB () {
+	// body...
+
+}
+
+//when creating an appraisal loading the icon path nd saving the actual icon in the file
+function loadIcon()
+{
+
+}
