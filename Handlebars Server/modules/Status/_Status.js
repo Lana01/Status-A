ff75c7aeea80ddb4f99c5680af371a35ee32559f
Tshepi <tshepimagagula@gmail.com>
2015-03-24 14:14:05 +0200

    Updated status.

--- Handlebars Server/modules/Status/_Status.js
+++ Handlebars Server/modules/Status/_Status.js
@@ -16,20 +16,15 @@ var ProfileSchema = mongoose.Schema({
 
 });
 
-
 ProfileSchema.methods.show = function(){
     console.log(this.userId + " " + this.moduleId + " " + this.status);
-}
-
+};
 var Profile = mongoose.model('Profile',ProfileSchema);
 
-
 var profile1 = new Profile({ userId: 'u12345678', moduleId:'COS332', status:0});
-
-
+var db = openDbConnection();
 
 function openDbConnection(){
-
 
 //Connect to the db.
     mongoose.connect('mongodb://45.55.154.156:27017/Buzz');
@@ -51,7 +46,6 @@ function closeDbConnection(db){
     });
 }
 
-var db = openDbConnection();
 /***********************************************************************/
 /****************************BuzzStatus_Class**************************/
 var BuzzStatus = function(){};
@@ -67,34 +61,31 @@ BuzzStatus.prototype.getStatusForProfile = function(getStatusForProfileRequest){
 BuzzStatus.prototype.setStatusCalculator = function(setStatusCalculatorRequest){
 
     var result = new getStatusForProfileResult();
-    result.status.statusValue = setStatusCalculatorRequest.user_status;
-    //updateStatusPointsForProfile();
+    result.statusValueAccessed = setStatusCalculatorRequest.accessmentValueResult;
+    updateStatusPointsForProfile(result.user_id,setStatusCalculatorRequest.module,result.statusValueAccessed);
     return result;
 };
 
-
 var setStatusCalculatorRequest = function(module_Id,AccessProfileResult){
 
-
     this.isOpen = false;
-
+    this.accessmentValueResult;
+    this.module = module_Id;
     db.collection('spaces').findOne({moduleID:module_Id}, function (err,data) {
 
         if(data.isOpen){
-            AccessProfileResult.accessmentValue;
+            this.accessmentValueResult = AccessProfileResult.accessmentValue;
         }
 
     });
-
-    return AccessProfileResult.accessmentValue;
 }
 
 setStatusCalculatorRequest.prototype.getProfileAccessor = function(AccessProfileResult){
     //ProfileAccessor to be used for calculation of the status.
 };
 
-
 var setStatusCalculatorResult = function(){
+    this.statusValueAccessed = 0.0;
 };
 
 var Status = function(){
@@ -109,12 +100,11 @@ Status.prototype.getStatusValue = function(){
     return this.statusValue;
 };
 
-
 var getStatusForProfileRequest = function(user_id,module_id){
     //Retrieve user and module id from database.
     var db = openDbConnection();
 
-    db.collection('Profile').findOne({userId:user_id,moduleId:module_id},function(err,data){
+    db.collection('profiles').findOne({userId:user_id,moduleId:module_id},function(err,data){
         console.log(user_id + " " + module_id);
     });
 
@@ -123,31 +113,27 @@ var getStatusForProfileRequest = function(user_id,module_id){
     this.user_status = 0.0;
 };
 
-
 var getStatusForProfileResult = function(){
 
     this.status = new Status();
     this.status.statusValue = 0;
 };
 
-function updateAllStatusPoints(user_Id,mod_Id,newStatus){
+function updateAllStatusPoints(user_Id,newStatus){
 
-    db.collection('spaces').findOne({userId:user_Id,moduleId:mod_Id}).update({status:newStatus});
+    console.log(newStatus);
+    db.collection('profiles').update({userId:user_Id},
+        {$set:{'status':newStatus}},function(err,result){
+    });
 }
 
 function updateStatusPointsForProfile(user_Id,mod_Id,newStatus){
-    db.collection('spaces').findOne({userId:user_Id,moduleId:mod_Id}).update({status:newStatus});
+
+    db.collection('profiles').update({userId:user_Id,moduleId:mod_Id},
+        {$set:{'status':newStatus}},function(err,result){
+        });
 }
 
-/*
-* Test
-* setStatusCalculatorRequest('zzz');
-* */
-
-
-
-
-
-
-
+//setStatusCalculatorRequest('zzz');
+//updateAllStatusPoints('u23456789','30');
 
