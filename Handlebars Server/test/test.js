e6863ccafde42f6e98d38fad288d4ae22d9623fa
Hanrich <Hanrichp@gmail.com>
2015-04-01 11:57:57 +0200

    test

---
+++ Handlebars Server/test/test.js
@@ -0,0 +1,23 @@
+var test = require('unit.js');
+var buzzStatus = require('../modules/Status/BuzzStatus');
+var profileAssessor = require('../modules/Status/ProfileAssessor');
+var Schemas = require('../modules/Status/Schemas');
+var appraisalType = require('../modules/Status/AppraisalType');
+
+describe('Automatic testing of Status A', function(){
+    // Here we test BuzzStatus
+    it('Buzz Status test 1', function(){
+
+    });
+    // Here we test ProfileAssessor
+    it('ProfileAssessor Test 1', function(){
+        var tmp = profileAssessor.
+
+
+        // other tests ...
+    });
+    // Here we test status
+    it('Status test 1', function(){
+        // other tests ...
+    });
+});
