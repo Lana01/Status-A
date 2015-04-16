e1b4a7d2738417a875a3479d8ab0f7749476ef91
Aipow <u12081622@tuks.co.za>
2015-04-16 19:08:40 +0200

    Unit Test cases that works on linux

--- Handlebars Server/test/test.js
+++ Handlebars Server/test/test.js
@@ -8,11 +8,12 @@ var Schemas = require("../modules/Status/Schemas");
 
 */
 var IoC = require('electrolyte');
-IoC.loader(IoC.node('node_modules'));
+IoC.loader(IoC.node('../node_modules'));
 var buzzStatus = IoC.create('buzz-status');
 
 console.log(buzzStatus);
 
+var test = require('unit.js');
 
 describe('Automatic testing of Status A', function(){
     // Here we test BuzzStatus
@@ -35,6 +36,7 @@ describe('Automatic testing of Status A', function(){
     });
     // Here we test ProfileAssessor
     it('ProfileAssessor Test 1', function(){
+
         //console.log("Testing..")
         //var tmp = profileAssessor.create('AppraisalsAssessor');
         //var request = {profileID:'ObjectId("5510932ec1df10641d5e9608")'};
@@ -51,7 +53,7 @@ describe('Automatic testing of Status A', function(){
     it('Test Appraisals db interaction ', function()
     {
 
-        //var tmp = new AssignAppraisalToPost(mongoose.Types.ObjectId("5513f8934af522f6583d9f15"), mongoose.Types.ObjectId(), mongoose.Types.ObjectId());
+       // var tmp = new buzzStatus.AssignAppraisalToPost(buzzStatus.mongoose.Types.ObjectId("5513f8934af522f6583d9f15"), buzzStatus.mongoose.Types.ObjectId(), mongoose.Types.ObjectId());
         //buzzStatus.assignAppraisalToPost(tmp);
 
         //var appraisalModel = mongoose.model('Appraisals', Schemas.appraisalSchema);
@@ -59,4 +61,33 @@ describe('Automatic testing of Status A', function(){
         //   if(err) throw err;
         //});
     });
+    it('Test Appraisals classes Appraisals ', function() {
+
+
+        var appraisalLevel1 = new buzzStatus.AppraisalLevel("Mildly Annoying", 1, "ICON_1");
+        var appraisalLevel2 = new buzzStatus.AppraisalLevel("SO Annoying", 2, "ICON_2");
+
+        var newAppraisalType = new buzzStatus.AppraisalType("Annoying", "This post is annoying", "NOT_RATED_ICON", [appraisalLevel1, appraisalLevel2]);
+
+        test.value(appraisalLevel1.name).isEqualTo("Mildly Annoying");
+        test.value(appraisalLevel1.rating).isEqualTo(1);
+        test.value(appraisalLevel1.icon).isEqualTo("ICON_1");
+
+        test.object(newAppraisalType)
+            .hasProperty('name')
+            .hasProperty('description')
+            .hasProperty('notRatedIcon')
+            .hasProperty('appraisalLevels')
+        ;
+
+
+    });
+    it('Test Appraisals activateAppraisalType ', function() {
+        var activateAppraisalTypeObj;
+       // activateAppraisalTypeObj= buzzStatus.activateAppraisalType({appraisalTypeID: "5513f7004af522f6583d9f14", spaceID: "COS 332"});
+
+
+    });
+
+
 });
